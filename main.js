6e72f14a05e4948d1592184935373099157a1e99
jag712 <95673048+jag712@users.noreply.github.com>
2025-09-11 21:04:24 +0900

    Update main.js

--- main.js
+++ main.js
@@ -605,13 +605,7 @@ function showModal() {
   const rotatingIcon = document.createElement("div");
   rotatingIcon.className = "rotating-icon-loader";
 
-  const cancelButton = document.createElement("button");
-  cancelButton.className = "mt-4 px-4 py-2 bg-gray-200 rounded hover:bg-gray-300";
-  cancelButton.textContent = "취소";
-  cancelButton.addEventListener("click", () => {
-    userAborted = true;
-    if (controller) controller.abort();
-    hideModal();
+ 
   });
 
   loadingContainer.appendChild(rotatingIcon);
